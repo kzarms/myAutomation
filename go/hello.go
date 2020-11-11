4cc373f6f3614e9dcabab9694be6f591c49ab565
Kot VSC <vlkoot@gmail.com>
2020-11-11 11:42:05 +0100

    Add first go sample

---
+++ go/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* This is my first sample program in GO. */
+	fmt.Println("Hello, World!")
+	fmt.Println("This is my code in GO!")
+}
+
+// C:\temp\Go\bin\go.exe run .\go\hello.go
+//https://www.tutorialspoint.com/go/go_variables.htm
