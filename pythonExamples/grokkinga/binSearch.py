4a99bbad43d248f298c3aaee887bcbcad5fad1b7
Konstantin <ikotpad@gmail.com>
2020-11-29 17:02:34 +0100

    Add samples

---
+++ pythonExamples/grokkinga/binSearch.py
@@ -0,0 +1,19 @@
+sList = list(range(1,101))
+
+
+def binSearch(a, sList):
+    low = 0
+    high = len(sList)-1
+    steps = 0
+    while low <= high:
+        steps += 1
+        mid = int((low + high) / 2)
+        guess = sList[mid]
+        if a == guess:            
+            return mid, steps
+        if guess > a:
+            high = mid
+        else:
+            low = mid
+
+print(binSearch(12, sList))
---
+++ pythonExamples/grokkinga/sorting.py
@@ -0,0 +1,39 @@
+# Selection sort
+arr = [5, 3, 4, 6, 16, 54, 23, 2, 44, 57, 65, 34]
+
+def SelectionSort(arr):
+    result = []
+    while arr:
+        myMin = arr[0]
+        for i in arr:
+            if i < myMin:
+                myMin = i
+        result.append(myMin)
+        arr.remove(myMin)
+    return result
+
+print(SelectionSort(arr))
+
+arr = [2, 4, 6]
+def recSum(arr):
+    if len(arr) == 1:
+        return arr[0]
+    else:
+        return arr[0]+recSum(arr[1:])
+           
+print(recSum(arr))
+
+
+### Qucick sort ###
+arr = [5, 3, 4, 6, 16, 54, 23, 2, 44, 57, 65, 34]
+
+def myQsort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return myQsort(less) + [pivot] + myQsort(greater)
+
+print(myQsort(arr))
