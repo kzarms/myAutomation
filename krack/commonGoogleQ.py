9e9e2019758fe4c32f63f3eee8008ded076aeb84
Kot VSC <vlkoot@gmail.com>
2020-12-25 16:03:53 +0100

    Add 2, 3, 4 solutions.

--- krack/commonGoogleQ.py
+++ krack/commonGoogleQ.py
@@ -66,4 +66,75 @@ class KthLargestNumberInStream:
 
 kthLN = KthLargestNumberInStream([3, 1, 5, 12, 2, 11], 4)
 
-###
+########################
+from heapq import *
+
+
+arr = [2, 4, 5, 6, 9, 13, 15]
+k = 3
+x = 6
+
+def find_closest_element(arr, k, x):
+    result = []
+
+    def binS(arr, x, start, end):
+
+        mid = int((start + end) / 2)
+
+        if arr[mid] == x:
+            return mid
+
+        if arr[mid] < x:
+            return binS(arr, x, mid+1, end)
+        else:
+            return binS(arr, x, start, mid-1)
+
+    index = binS(arr, x, 0, 5)
+
+    for i in range(index - 2, index + 3):
+        heappush(result, arr[i])
+
+    return result[0:3]
+
+print(find_closest_element(arr, k, x))
+
+
+#### Summ of two values
+
+A = [2, 7, 11, 15]
+val = 9
+
+def find_sum_of_two(A, val):
+    h = {}
+    for i in range(len(A)):
+        x = val - A[i]
+        if x in h:
+            return h[x], i
+        else:
+            h[A[i]] = i
+    return None
+
+
+print(find_sum_of_two(A, val))
+
+
+### Delete node with a given key
+
+head = [4, 5, 1, 9]
+key = 5
+
+def delete_node(head, key):
+    prev = 0
+    if key == head[0]:
+        return head[1:]
+
+    for i in range(len(head)):
+        if head[i] == key:
+            return head[0:prev + 1] + head[i + 1:]
+        else:
+            prev = i
+    return head
+
+print(delete_node(head, key))
+
+# https://www.educative.io/blog/google-coding-interview-questions
