6ec1c3fde5cfa038b95d390d97ca11770b5c1f94
Kot VSC <vlkoot@gmail.com>
2020-12-29 09:21:39 +0100

    Add bubble sort + optimized version

--- krack/sorting.py
+++ krack/sorting.py
@@ -1,4 +1,4 @@
-## Selection sort
+## Selection sort n^2
 
 arr = [64, 25, 12, 22, 11]
 
@@ -29,4 +29,38 @@ def selectSort2(arr):
 
 print(selectSort2(arr))
 
+# Bubble sort n^2
+# My implementation
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+def myBubbleSort(arr):
+    temp_arr = arr[:]
+    swipe = True
+    while swipe:
+        swipe = False
+        for i in range(len(temp_arr)-1):
+            if temp_arr[i] > temp_arr[i+1]:
+                swipe = True
+                temp_arr[i], temp_arr[i+1] = temp_arr[i+1], temp_arr[i]
+    return temp_arr
+
+print(myBubbleSort(arr))
+
+# Optimized implementation with two loops
+
+
+def bubbleSort(arr):
+    temp_arr = arr[:]
+    n = len(temp_arr)
+
+    for i in range(n):
+        # last i elements are in plase already
+        for j in range(0, n-i-1):
+            if temp_arr[j] > temp_arr[j+1]:
+                temp_arr[j], temp_arr[j+1] = temp_arr[j+1], temp_arr[j]
+
+    return temp_arr
+
+print(bubbleSort(arr))
 
