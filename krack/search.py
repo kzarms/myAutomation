0b567fb91766db9d944e1ee87df8f5dfe36c8fe6
Kot VSC <vlkoot@gmail.com>
2021-01-02 13:51:04 +0100

    add binary search in sorted array.

--- krack/search.py
+++ krack/search.py
@@ -11,3 +11,27 @@ def lsearch(arr, x):
 
 print(lsearch(arr, x))
 print(lsearch(arr, 60))
+
+
+## BTS ###
+
+arr = [2, 3, 4, 10, 40, 50, 55, 70, 75, 80, 91, 94, 99]
+
+def binSearch(arr, x):
+
+    l = arr[0]
+    r = len(arr) - 1
+
+    while l <= r:
+        mid = l + (r - l)//2
+        if arr[mid] == x:
+            return mid
+
+        elif arr[mid] < x:
+            l = mid + 1
+        else:
+            r = mid - 1
+
+    return -1
+
+print(binSearch(arr, 10))
