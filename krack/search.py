7675d70b213afafa8f1c3720270da65015cad07c
Kot VSC <vlkoot@gmail.com>
2021-01-02 14:53:54 +0100

    Add jump search

--- krack/search.py
+++ krack/search.py
@@ -1,4 +1,4 @@
-### Liniar search ###
+### Liniar search ### O(n)
 
 arr = [10, 20, 30, 80, 50, 70, 40, 90]
 x = 80
@@ -13,7 +13,7 @@ print(lsearch(arr, x))
 print(lsearch(arr, 60))
 
 
-## BTS ###
+## BTS ### O(log(n))
 
 arr = [2, 3, 4, 10, 40, 50, 55, 70, 75, 80, 91, 94, 99]
 
@@ -35,3 +35,28 @@ def binSearch(arr, x):
     return -1
 
 print(binSearch(arr, 10))
+
+
+#### jump search ##### O(sqrt(n))
+arr = [2, 3, 4, 10, 40, 50, 55, 70, 75, 80, 91, 94, 99, 101, 123, 144]
+
+def jsearch(arr, x):
+
+    jump = int(len(arr)**(1/2))
+    i = 0
+    while i <= (len(arr) -1) and arr[i] <= x:
+        if arr[i] == x:
+            return i
+        elif arr[i] < x:
+            i = i + jump
+        else:
+            break
+
+    # i has the index of value > than x
+    for j in range(i, i - jump, -1):
+        if arr[j] == x:
+            return j
+
+    return -1
+
+print(jsearch(arr, x))
