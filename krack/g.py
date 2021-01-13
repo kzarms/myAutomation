358041bb852e256a9fcee9f6573ff0dc3ad6ab47
Kot VSC <vlkoot@gmail.com>
2021-01-13 11:38:26 +0100

    Add O(1) notation to the code.

--- krack/g.py
+++ krack/g.py
@@ -48,6 +48,73 @@ def foo2(x, y, p):
 
 print(foo2(2, 3, 5))
 
+####### Find the largest word in dict ###########
+
+myDic = ["ale", "apple", "monkey", "plea"]
+mySrt = "abpcplea"
 
 
+def findLargestWord(myDic, myStr):
 
+    basicHash = {}
+    for i in myStr:
+        if i in basicHash:
+            basicHash[i] += 1
+        else:
+            basicHash[i] = 1
+
+    for el in myDic:
+        tempHash = basicHash.copy()
+
+        for i in el:
+            if i in tempHash:
+                tempHash -= 1
+
+
+def constructString(dp, n, bCnt=1, cCnt=2):
+    if bCnt < 0 or cCnt < 0:
+        return 0
+
+    if n == 0:
+        return 1
+    if bCnt == 0 and cCnt == 0:
+        return 1
+
+    if dp[n][bCnt][cCnt] != -1:
+        return dp[n][bCnt][cCnt]
+
+    res = constructString(dp, n-1, bCnt, cCnt)
+    res += constructString(dp, n-1, bCnt-1, cCnt-1)
+    res += constructString(dp, n-1, bCnt, cCnt-1)
+
+    dp[n][bCnt][cCnt] = res
+
+    return dp[n][bCnt][cCnt]
+
+def contSrt(n):
+    dp = [[[-1 for x in range(n+2)] for y in range(3)] for z in range(4)]
+    return constructString(dp, n)
+
+print(contSrt(3))
+
+
+################# O(1) #################
+# https://careercup.appspot.com/question?id=5717453712654336
+
+def contSrt2(n):
+
+    res = 0
+    # C = 0
+    res += 1 # no B
+    res += n # B on each position
+    # C = 1
+    res += 1 * n
+    res += (n - 1)*n
+
+    # C = 2
+    res += 1*(n-1)*n/2
+    res += (n-2)*(n*(n-1)/2)
+
+    return res
+
+print(contSrt2(3))
